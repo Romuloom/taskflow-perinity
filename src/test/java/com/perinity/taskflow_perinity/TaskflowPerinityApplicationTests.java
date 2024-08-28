deae729aecf9432763451021ec5a914029b3c4c3
Romulo <romulomesquita.06@gmail.com>
2024-08-28 17:43:51 -0300

    adição do docker e ajuste no arquivo application

---
+++ src/test/java/com/perinity/taskflow_perinity/TaskflowPerinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perinity.taskflow_perinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowPerinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
